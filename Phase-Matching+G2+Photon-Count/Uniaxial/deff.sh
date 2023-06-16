aa56d68dc1f12a92d1bf80dd8ad8e377e37e56a5
Sanoj <134020657+snoozynooj@users.noreply.github.com>
2023-06-16 00:17:39 -0400

    Add files via upload

---
+++ Phase-Matching+G2+Photon-Count/Uniaxial/deff.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+input="database"
+#for folders in $list1;do
+while IFS= read -r line
+do
+    echo $line > result
+    cif=$(awk '{print $1}' result)
+    crystaltype=$(awk '{print $2}' result)
+    bandgap=$(awk '{print $3}' result)
+    signal=$(awk '{print $4}' result)
+
+    cp autophasematching.py.bak $cif-autophasematching.py
+    sed -i "s/FILE/$cif/" $cif-autophasematching.py
+    sed -i "s/bandgap/$bandgap/" $cif-autophasematching.py
+    sed -i "s/PUMP/$signal/" $cif-autophasematching.py
+    echo "$cif $bandgap $signal"
+#    python $cif-autophasematching.py > $cif-Count.txt 
+    python $cif-autophasematching.py  
+done < "$input"
+#rm *-au*
+#rm *-G2*
